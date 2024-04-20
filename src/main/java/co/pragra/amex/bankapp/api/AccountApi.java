df509ce1367953002fbff6c068e2a09d6d07aa28
Atin Singh <atin@pragra.io>
2024-04-20 12:51:35 -0400

    Updated account code

---
+++ src/main/java/co/pragra/amex/bankapp/api/AccountApi.java
@@ -0,0 +1,26 @@
+package co.pragra.amex.bankapp.api;
+
+import co.pragra.amex.bankapp.entity.Account;
+import co.pragra.amex.bankapp.service.AccountService;
+import co.pragra.amex.bankapp.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/customer")
+public class AccountApi {
+
+    private final AccountService accountService;
+
+    public AccountApi(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/{uuid}/account")
+    public ResponseEntity<Account> createAccount(@RequestBody Account account, @PathVariable String uuid) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(accountService.createAccount(account,UUID.fromString(uuid)));
+    }
+}
--- src/main/java/co/pragra/amex/bankapp/service/AccountService.java
+++ src/main/java/co/pragra/amex/bankapp/service/AccountService.java
@@ -1,9 +1,35 @@
 package co.pragra.amex.bankapp.service;
 
+import co.pragra.amex.bankapp.entity.Account;
+import co.pragra.amex.bankapp.entity.Customer;
+import co.pragra.amex.bankapp.exceptions.NoDataFoundException;
 import co.pragra.amex.bankapp.repo.AccountRepo;
+import co.pragra.amex.bankapp.repo.CustomerRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class AccountService {
-    private AccountRepo accountRepo;
+    private final AccountRepo accountRepo;
+    private final CustomerRepo customerRepo;
+
+    public AccountService(AccountRepo accountRepo, CustomerRepo customerRepo) {
+        this.accountRepo = accountRepo;
+        this.customerRepo = customerRepo;
+    }
+
+    public Account createAccount(Account account, UUID uuid){
+        if(uuid == null) {
+            throw  new RuntimeException("UUID can't be null");
+        }
+        Customer customer = customerRepo.findById(uuid).orElseThrow(() -> new NoDataFoundException("Customer with UUID" + uuid.toString() + " not found"));
+        Account saved = accountRepo.save(account);
+        List<Account> accounts = customer.getAccounts();
+        accounts.add(saved);
+        customer.setAccounts(accounts);
+        customerRepo.save(customer);
+        return saved;
+    }
 }
